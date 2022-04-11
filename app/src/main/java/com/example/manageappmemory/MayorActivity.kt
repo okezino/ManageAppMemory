549a734051ecb87998539467be074aa6e634d53b
okezino <okezi002@gmail.com>
2022-04-11 16:43:09 +0100

    dependecies

---
+++ app/src/main/java/com/example/manageappmemory/MayorActivity.kt
@@ -0,0 +1,11 @@
+package com.example.manageappmemory
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MayorActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mayor)
+    }
+}
